668d7c28e13a7c141f78ba46cf3739b6caabe6e3
Julia Menschikova <yu.menschikowa2015@yandex.ru>
2023-10-18 15:20:30 +0300

    add ParsingJsonTest

--- src/test/java/tests/OpenZipTest.java
+++ src/test/java/tests/OpenZipTest.java
@@ -6,7 +6,6 @@ import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -40,7 +39,7 @@ public class OpenZipTest {
     @Test
     void xslTest() throws Exception {
         try (InputStream stream = cl.getResourceAsStream("resources.zip");
-             ZipInputStream zip= new ZipInputStream(stream)) {
+             ZipInputStream zip = new ZipInputStream(stream)) {
             ZipEntry entry;
             while ((entry = zip.getNextEntry()) != null) {
                 if (entry.getName().contains(".xls")) {
@@ -48,22 +47,7 @@ public class OpenZipTest {
                     Assertions.assertEquals("Парфюмерия >> Нишевая парфюмерия >> Parfums De Marly",
                             xls.excel.getSheetAt(0).getRow(46).
                                     getCell(0).getStringCellValue());
-                } else if (entry.getName().contains(".csv")) {
-                    Reader reader = new InputStreamReader(zip);
-                    CSVReader csvReader = new CSVReader(reader);
-                    List<String[]> content = csvReader.readAll();
-
-                    Assertions.assertEquals(2, content.size());
-
-                    final String[] first = content.get(0);
-                    final String[] second = content.get(1);
-
-                    Assertions.assertArrayEquals(new String[]{"Mancera", "Hindu kush"}, first);
-                    Assertions.assertArrayEquals(new String[]{"Xerjoff", "Naxos"}, second);
-
-
                 }
-
             }
         }
     }
